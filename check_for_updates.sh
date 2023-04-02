bf2d683ae56b36dbedc9fdc910e4ca3c4d329226
LtFubuki <99317076+LtFubuki@users.noreply.github.com>
2023-04-02 16:39:03 -0700

    Update check_for_updates.sh

--- check_for_updates.sh
+++ check_for_updates.sh
@@ -2,25 +2,26 @@
 
 # Set repository URL and local directories
 REPO_URL="https://github.com/LtFubuki/site-1/archive/refs/heads/main.zip"
-LOCAL_DIR="/site-1"
+LOCAL_DIR="/home/pi/site-1"
+LOCAL_WEB_DIR="${LOCAL_DIR}/web"
 TMP_DIR="/tmp/site-1-main"
 
-# Enter the local directory
-cd "$LOCAL_DIR"
+# Create the local 'web' directory if it doesn't exist
+mkdir -p "$LOCAL_WEB_DIR"
 
 # Run the script in an infinite loop
 while true; do
   # Download the GitHub repository to a temporary folder
-  wget -q -O main.zip "$REPO_URL"
-  unzip -q -o main.zip -d /tmp
-  rm -f main.zip
+  wget -q -O /tmp/main.zip "$REPO_URL"
+  unzip -q -o /tmp/main.zip -d /tmp
+  rm -f /tmp/main.zip
 
   # Compare the 'web' folder in the temporary folder with the local folder
-  if ! diff -qr "$TMP_DIR/web" "web"; then
+  if ! diff -qr "$TMP_DIR/web" "$LOCAL_WEB_DIR"; then
     echo "Changes detected. Updating the local folder and restarting the static_website service."
 
     # Copy the updated 'web' folder to the local folder
-    cp -r "$TMP_DIR/web" .
+    cp -r "$TMP_DIR/web" "$LOCAL_DIR"
 
     # Restart the static_website service in Docker Compose
     docker-compose restart static_website
