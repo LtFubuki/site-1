54672e1f3968272f8df13d0304696b08bab90b7e
LtFubuki <99317076+LtFubuki@users.noreply.github.com>
2023-04-02 13:55:48 -0700

    Update update-website.sh

--- update-website.sh
+++ update-website.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 # Set variables
-REPO_URL="https://github.com/LtFubuki/site-1"
-WEB_DIR="web"
+REPO_URL="https://github.com/LtFubuki/site-1/archive/refs/heads/main.zip"
 TEMP_DIR="temp_update"
 NGINX_HTML_DIR="html"
 
 # Create the temp directory if it doesn't exist
 mkdir -p ${TEMP_DIR}
 
-# Download the web files to the temp dir
-wget -q --show-progress --recursive --no-parent --no-clobber --no-check-certificate --directory-prefix=${TEMP_DIR} ${REPO_URL}/raw/main/${WEB_DIR}/
+# Download the repository zip file
+wget -q --show-progress --no-check-certificate -O ${TEMP_DIR}/main.zip ${REPO_URL}
+
+# Unzip the repository files
+unzip -qo ${TEMP_DIR}/main.zip -d ${TEMP_DIR}
 
 # Compare and update the web files if there's a change
-if ! rsync -rucn --exclude '.git' ${TEMP_DIR}raw/main/${WEB_DIR}/ ${NGINX_HTML_DIR}/ | grep -q '^'; then
+if ! rsync -rucn --exclude '.git' ${TEMP_DIR}/site-1-main/web/ ${NGINX_HTML_DIR}/ | grep -q '^'; then
     echo "Updating website..."
     
     # Update the web files
-    rsync -ruc --exclude '.git' ${TEMP_DIR}raw/main/${WEB_DIR}/ ${NGINX_HTML_DIR}/
+    rsync -ruc --exclude '.git' ${TEMP_DIR}/site-1-main/web/ ${NGINX_HTML_DIR}/
 
     # Restart the web container to apply the changes
     docker-compose restart web
